82750bcd5938cff43213f4d43e02c7a63d06203c
CasperHogstrom <casperhogstrom3@gmail.com>
2022-09-15 14:51:50 +0200

    Exercise 1 and 2 is done, and 3 is about halfway done

--- public/exercises.js
+++ public/exercises.js
@@ -2,8 +2,45 @@ console.log('Hello world');
 
 function exercise1() {
     console.log('Wassaaaaap');
-    var txtBox1Value = document.getElementById('txt1').value;
-    var txtBox2Value = document.getElementById('txt2').value;
+    var textBoxValue = document.getElementById('txt').value;
+    const words = textBoxValue.split(' ');
+    console.log(words.length);
 
-    console.log(`${txtBox1Value}, ${txtBox2Value} ${txtBox1Value.length}`);
+    words.forEach(word => {
+        console.log(`${word} (${word.length})`)
+    });
+}
+
+function exercise2() {
+    var textBoxValue = document.getElementById('txt').value;
+    const words = textBoxValue.split(' ');
+    const words2 = textBoxValue.split(' ');
+
+    console.log(words.sort((a, b) => b.length - a.length));
+    words.forEach(word => {
+        console.log(`${word}`)
+    });
+    
+    console.log(words2.reverse());
+    words2.forEach(word => {
+        console.log(`${word}`)
+    });
+
+}
+
+function exercise3() {
+    var textBoxValue = document.getElementById('txt').value;
+    const words = textBoxValue.split(' ');
+
+    obj = {};
+    
+        for(var i = 0; i < words.length; i++) {
+            if (obj[words[i]] == undefined) {
+                obj[words[i]] = 1;
+            } else {
+                obj[words[i]]++;
+            }
+        }
+
+    Object.values(obj).forEach((freq, word) => console.log(`${words[word]} ${freq}`));
 }
