55e71cdac793c08d4a4367738e1882cbc0a7c829
CasperHogstrom <casperhogstrom3@gmail.com>
2022-09-15 10:18:53 +0200

    structure in place redy to go

---
+++ public/exercises.js
@@ -0,0 +1,9 @@
+console.log('Hello world');
+
+function exercise1() {
+    console.log('Wassaaaaap');
+    var txtBox1Value = document.getElementById('txt1').value;
+    var txtBox2Value = document.getElementById('txt2').value;
+
+    console.log(`${txtBox1Value}, ${txtBox2Value} ${txtBox1Value.length}`);
+}
